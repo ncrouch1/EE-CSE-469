800c346f3123bed9556f7062243f602575c702de
Tyler <tyjhittl@cs.washington.edu>
2023-04-01 10:46:10 -0700

    Register File Added!

---
+++ Lab1/reg_file.sv
@@ -0,0 +1,11 @@
+module reg_file(input logic clk, wr_en,
+	input logic [31:0] write_data,
+	input logic [3:0] write_addr, input logic [3:0]
+	read_addr1, read_addr2, output logic [31:0]
+	read_data1, read_data2);
+	
+	
+
+	
+	
+endmodule
