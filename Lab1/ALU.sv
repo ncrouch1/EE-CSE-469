1c35aa05667bd5a156ba05c08c3bcfb95a5ac55a
Tyler <tyjhittl@cs.washington.edu>
2023-04-03 14:59:13 -0700

    TT and ALU are happy now

--- Lab1/ALU.sv
+++ Lab1/ALU.sv
@@ -1,47 +1,62 @@
-module ALU(A, B, CTRL, Result, Flags);
+module ALU(a, b, ALUControl, Result, ALUFlags);
 	
 	// set up
-	input logic [31:0] A, B;
-	input logic [1:0] CTRL;
+	input logic [31:0] a, b;
+	input logic [1:0] ALUControl;
 	output logic [31:0] Result;
-	output logic [3:0] Flags;
+	output logic [3:0] ALUFlags;
 	
 	logic cout;
-	logic ctrl;
-	assign ctrl = CTRL[0];
+	logic sub;
+	logic [31:0] orbus, andbus, mathbus;
+	assign sub = CTRL[0];
 	
+	genvar i;
 	// perform
-	
+	// Math operations driven by the chain of fulladders
+	ttLogic addsub (.a(a), .b(b), .sum(mathbus), .ctrl(sub), .cout(cout));
+
+	// logic for and/or
 	generate
-		case(CTRL[0])
-		// Add
-			1'b0 : begin : add
-				ttLogic ttadd (.a(A), .b(B), .sum(Result), .ctrl(ctrl), .cout(cout));
-			end
-			
-			// Sub
-			1'b1 : begin : sub
-				ttLogic ttsub (.a(A), .b(B), .sum(Result), .ctrl(ctrl), .cout(cout));
-			end		
-		endcase
+	for (i = 0; i < 32; i++ ) begin
+		andbus[i] = (a[i] & b[i]);
+		orbus[i] = (a[i] | b[i]);
+	end
 	endgenerate
 	
 	always_comb begin
-		case(CTRL)
-			// And
-			2'b10 : begin : aand
-				for (i = 0; i < 32; i++) begin 
-					Result[i] = (A[i] & B[i]);
-				end
+		case(ALUControl) 
+			2'b00 : begin
+				Result = mathbus;
 			end
-			
-			// Or
-			2'b11 : begin : orr
-				for (i = 0; i < 32; i++) begin 
-					Result[i] = (A[i] | B[i]);
-				end
+			2'b01 : begin
+				Result = mathbus;
+			end
+			2'b10 : begin
+				Result = andbus;
+			end
+			2'b11 : begin
+				Result = orbus;
 			end
 		endcase
+	end
+	
+//	always_comb begin
+//		case(CTRL)
+//			// And
+//			2'b10 : begin : aand
+//				for (i = 0; i < 32; i++) begin 
+//					Result[i] = (A[i] & B[i]);
+//				end
+//			end
+//			
+//			// Or
+//			2'b11 : begin : orr
+//				for (i = 0; i < 32; i++) begin 
+//					Result[i] = (A[i] | B[i]);
+//				end
+//			end
+//		endcase
 	end
 endmodule
 		
--- Lab1/ttLogic.sv
+++ Lab1/ttLogic.sv
@@ -8,23 +8,14 @@ module ttLogic(a, b, sum, ctrl, cout);
 	logic [32:0] cry;
 	assign cry[0] = ctrl;
 	
+	logic [31:0] btemp;
+	assign btemp = ctrl ? b : ~b;
+	
 	genvar i;
 	generate 
-		case(ctrl)
-		
-			1'b0 : begin : add
-				for (i = 0; i < 32; i++) begin : logi
-					fullAdder adder(.A(a[i]), .B(b[i]), .cin(cry[i]), .sum(sum[i]), .cout(cry[i+1]));
-				end
-			end
-			
-			1'b1 : begin : sub
-				for (i = 0; i < 32; i++) begin : logi
-					fullAdder adder(.A(a[i]), .B(~b[i]), .cin(cry[i]), .sum(sum[i]), .cout(cry[i+1]));
-				end
-			end
-			
-		endcase		
+		for (i = 0; i < 32; i++) begin : logi
+			fullAdder adder(.A(a[i]), .B(btemp[i]), .cin(cry[i]), .sum(sum[i]), .cout(cry[i+1]));
+		end
 	endgenerate
 	
 	assign cout = cry[32];
