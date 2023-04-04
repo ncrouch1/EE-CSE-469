9bfb8a7550502055f08d79c5933900392b97638a
Tyler <tyjhittl@cs.washington.edu>
2023-04-04 15:18:39 -0700

    Reg_file tb done

--- Lab1/reg_file.sv
+++ Lab1/reg_file.sv
@@ -18,7 +18,57 @@ module reg_file(input logic clk, wr_en,
 endmodule
 
 module reg_file_tb ();
+logic [31:0] write_data;
+logic [3:0] write_addr; 
+logic [3:0] read_addr1, read_addr2;
+logic [31:0] read_data1, read_data2;
+logic clk, wr_en;
+
+reg_file dut (clk, wr_en, write_data, write_addr, read_addr1, read_addr2, read_data1, read_data2);
 
 
+initial begin
+	clk = 0;
+	wr_en = 0;
+	write_data = 8675309;
+	write_addr = 3;
+	read_addr1 = 3;
+	#10;
+	clk = 1;
+	#10;
+	clk = 0;
+	#5;
+	read_addr1 = 2;
+	read_addr2 = 3;
+	#5;
+	clk = 1;
+	#10;
+	clk = 0;
+	wr_en = 1;
+	#5;
+	read_addr1 = 3;
+	read_addr2 = 0;
+	#5;
+	clk = 0;
+	#10;
+	clk = 1;
+	#10;
+	read_addr2 = 3;
+	read_addr1 = 2;
+	#10;
+	clk = 0;
+	wr_en = 0;
+	write_addr = 4;
+	write_data = 420;
+	read_addr2 = 4;
+	#10;
+	clk = 1;
+	#10;
+	clk = 0;
+	wr_en = 1;
+	#10;
+	clk = 1;
+	#10;
+end
 
 endmodule
