9ebf063ed085a77839d0a32e28134794a89af2c8
Tyler <tyjhittl@cs.washington.edu>
2023-04-04 15:47:58 -0700

    commented

--- Lab1/reg_file.sv
+++ Lab1/reg_file.sv
@@ -3,12 +3,12 @@ module reg_file(input logic clk, wr_en,
 	input logic [3:0] write_addr, input logic [3:0]
 	read_addr1, read_addr2, output logic [31:0]
 	read_data1, read_data2);
-	
+	// Memory is created below
 	logic [15:0][31:0] memory;
-
+	// since reads are asynchronous, we can just use assign here
 	assign read_data1 = memory[read_addr1];
 	assign read_data2 = memory[read_addr2];
-
+	// on clockedge, we write to the spot if wr_en is high
 	always_ff @(posedge clk) begin
 		if (wr_en) begin
 			memory[write_addr] <= write_data;
