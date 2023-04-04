9d7d9f18687c5df1f973700d928e5ce5eda43d38
Noah Crouch <ncrouch1@uw.edu>
2023-04-04 12:09:18 -0700

    Got ALU to compile. Added ALUFlags as well and those are compiling as well.

--- Lab1/ALU.sv
+++ Lab1/ALU.sv
@@ -9,19 +9,19 @@ module ALU(a, b, ALUControl, Result, ALUFlags);
 	logic cout;
 	logic sub;
 	logic [31:0] orbus, andbus, mathbus;
-	assign sub = CTRL[0];
+	assign sub = ALUControl[0];
 	
-	genvar i;
 	// perform
 	// Math operations driven by the chain of fulladders
 	ttLogic addsub (.a(a), .b(b), .sum(mathbus), .ctrl(sub), .cout(cout));
 
+	genvar i;
 	// logic for and/or
 	generate
-	for (i = 0; i < 32; i++ ) begin
-		andbus[i] = (a[i] & b[i]);
-		orbus[i] = (a[i] | b[i]);
-	end
+		for (i = 0; i < 32; i++ ) begin : Logic
+			assign andbus[i] = (a[i] & b[i]);
+			assign orbus[i] = (a[i] | b[i]);
+		end
 	endgenerate
 	
 	always_comb begin
@@ -41,22 +41,21 @@ module ALU(a, b, ALUControl, Result, ALUFlags);
 		endcase
 	end
 	
-//	always_comb begin
-//		case(CTRL)
-//			// And
-//			2'b10 : begin : aand
-//				for (i = 0; i < 32; i++) begin 
-//					Result[i] = (A[i] & B[i]);
-//				end
-//			end
-//			
-//			// Or
-//			2'b11 : begin : orr
-//				for (i = 0; i < 32; i++) begin 
-//					Result[i] = (A[i] | B[i]);
-//				end
-//			end
-//		endcase
-	end
+	assign ALUFlags[3] = Result[31];
+	
+	logic[32:0] zeroTest;
+	assign zeroTest[0] = 1;
+	genvar j;
+	generate 
+		for (j = 0; j < 32; j++) begin : Test_for_zero
+			assign zeroTest[j+1] = zeroTest[j] & ~Result[j];
+		end
+	endgenerate
+	
+	assign ALUFlags[2] = zeroTest[32];
+	
+	assign ALUFlags[1] = (~ALUControl[1] & cout);
+	assign ALUFlags[0] = (~ALUControl[1] & ((a[31] & ~Result[31]) || (~a[31] & Result[31])) &
+					~(a[31] & b[31] & ALUControl[0]));
 endmodule
 		
