9d65ee69f461107062f51f84cf78d0d3a604edfc
Tyler <tyjhittl@cs.washington.edu>
2023-04-21 21:59:55 -0700

    Wooooo

--- Lab2/FlagsReg.sv
+++ Lab2/FlagsReg.sv
@@ -1,3 +1,10 @@
+/* This module holds our flags in dedicated memory to facilitate
+	conditional jumps.
+	FlagsIn is our flags input
+	clk is the clock signal
+	FlagW tells the circuit when to update the flag registers
+	FlagsOut outputs the currently saved flags.
+*/
 module FlagsReg ( FlagsIn, FlagW, clk, FlagsOut);
 	input logic [3:0] FlagsIn;
 	input logic FlagW;
@@ -5,7 +12,8 @@ module FlagsReg ( FlagsIn, FlagW, clk, FlagsOut);
 	output logic [3:0] FlagsOut;
 	
 	logic [3:0] memory;
-	
+	// On clock edge, update the saved flags if we are told to 
+	// write flags.
 	always_ff @(posedge clk) begin
 		if(FlagW)
 			memory <= FlagsIn;
